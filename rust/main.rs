493a476d0b3051995a7b5a601846ca3643cb753d
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2025-04-12 18:58:18 +0100

    Allow multiple queries

--- rust/main.rs
+++ rust/main.rs
@@ -2,21 +2,31 @@ use std::io;
 
 #[tokio::main]
 async fn main() {
-    println!("Enter a Pokémon species name:");
-    let mut species = String::new();
+    println!("Enter a Pokémon species name (or `exit`):");
+    let exit = String::from("exit\n");
+    let rustemon_client: rustemon::client::RustemonClient =
+        rustemon::client::RustemonClient::default();
 
-    io::stdin()
-        .read_line(&mut species)
-        .expect("Failed to read line");
+    loop {
+        let mut species = String::new();
 
-    let rustemon_client = rustemon::client::RustemonClient::default();
-    let species_stats = rustemon::pokemon::pokemon::get_by_name(&species, &rustemon_client).await;
-    match species_stats {
-        Ok(species) => {
-            for stat in species.stats.into_iter() {
-                println!("{}: {}", stat.stat.name, stat.base_stat);
-            }
+        io::stdin()
+            .read_line(&mut species)
+            .expect("Failed to read line");
+
+        if species.eq_ignore_ascii_case(&exit) {
+            break;
         }
-        Err(_) => println!("Not a valid species name"),
+
+        let species_stats =
+            rustemon::pokemon::pokemon::get_by_name(&species, &rustemon_client).await;
+        match species_stats {
+            Ok(species) => {
+                for stat in species.stats.into_iter() {
+                    println!("{}: {}", stat.stat.name, stat.base_stat);
+                }
+            }
+            Err(_) => println!("Not a valid species name"),
+        }
     }
 }
