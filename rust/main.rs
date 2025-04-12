72b73288984ded36a097d06efd368fe9458229bf
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2025-04-12 18:58:18 +0100

    Initial implementation

--- rust/main.rs
+++ rust/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+#[tokio::main]
+async fn main() {
+    println!("Enter a Pokémon species name:");
+    let mut species = String::new();
+
+    io::stdin()
+        .read_line(&mut species)
+        .expect("Failed to read line");
+
+    let rustemon_client = rustemon::client::RustemonClient::default();
+    let pokemon = rustemon::pokemon::pokemon::get_by_name(&species, &rustemon_client).await;
+    println!("{}", pokemon.as_ref().unwrap().name);
+    for stat in pokemon.unwrap().stats.into_iter() {
+        println!("{}: {}", stat.stat.name, stat.base_stat);
+    }
+    
 }
