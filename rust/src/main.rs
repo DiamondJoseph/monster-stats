b52627f79b771369539ca3623cb50760efba43b2
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2025-04-22 14:52:30 +0100

    feat: Track instances of monster species
    closes #10

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,33 +1,57 @@
+use std::collections::HashMap;
 use std::io;
 
 #[tokio::main]
 async fn main() {
     let exit = String::from("exit\n");
+    let caught = String::from("y\n");
     let rustemon_client: rustemon::client::RustemonClient =
         rustemon::client::RustemonClient::default();
+    let mut pokemon_box = HashMap::new();
 
     loop {
         println!("Enter a species name (or `exit`):");
-        let mut input = String::new();
+        let mut species_input = String::new();
 
         io::stdin()
-            .read_line(&mut input)
+            .read_line(&mut species_input)
             .expect("Failed to read line");
 
-        if input.eq_ignore_ascii_case(&exit) {
+        if species_input.eq_ignore_ascii_case(&exit) {
             break;
         }
 
-        let result = rustemon::pokemon::pokemon::get_by_name(&input, &rustemon_client).await;
+        let result =
+            rustemon::pokemon::pokemon::get_by_name(&species_input, &rustemon_client).await;
         match result {
             Ok(species) => {
-                for stat in species.stats.into_iter() {
+                for stat in species.stats.iter() {
                     println!("{}: {}", stat.stat.name, stat.base_stat);
+                }
+
+                println!("Did you catch it? (Y/N):");
+                let mut caught_input = String::new();
+
+                io::stdin()
+                    .read_line(&mut caught_input)
+                    .expect("Failed to read line");
+
+                if !caught_input.eq_ignore_ascii_case(&caught) {
+                    println!("Better luck next time");
+                } else {
+                    println!("Enter a name for your new {}", species.name);
+                    let mut name_input = String::new();
+
+                    io::stdin()
+                        .read_line(&mut name_input)
+                        .expect("Failed to read line");
+
+                    pokemon_box.insert(name_input, species);
                 }
             }
             Err(err) => println!(
                 "Unable to fetch {} ({}): is the species name correct?",
-                input, err
+                species_input, err
             ),
         }
         println!()
