2a9df1d122972e928c90bde260e4cdb650ccfb2a
Joseph Ware <53935796+DiamondJoseph@users.noreply.github.com>
2025-04-25 12:18:05 +0100

    feat: Track instances of monster species (#30)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,35 +1,90 @@
+use std::collections::HashMap;
 use std::io;
+
+use rustemon::client::RustemonClient;
+use rustemon::model::pokemon::Pokemon;
+use rustemon::pokemon::pokemon;
+use strum_macros::EnumString;
+
+#[derive(EnumString)]
+#[strum(ascii_case_insensitive)]
+enum Caught {
+    Y,
+    N,
+}
+
+#[derive(EnumString)]
+#[strum(ascii_case_insensitive)]
+enum Funcs {
+    Encounter,
+    Withdraw,
+    Exit,
+}
 
 #[tokio::main]
 async fn main() {
-    let exit = String::from("exit\n");
-    let rustemon_client: rustemon::client::RustemonClient =
-        rustemon::client::RustemonClient::default();
+    let rustemon_client = RustemonClient::default();
+    let mut pokemon_box = HashMap::new();
 
     loop {
-        println!("Enter a species name (or `exit`):");
-        let mut input = String::new();
+        let function = read_in("Encounter, Withdraw or Exit?");
 
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-
-        if input.eq_ignore_ascii_case(&exit) {
-            break;
+        match function.parse() {
+            Ok(Funcs::Encounter) => encounter(&rustemon_client, &mut pokemon_box).await,
+            Ok(Funcs::Withdraw) => withdraw(&pokemon_box),
+            Ok(Funcs::Exit) => break,
+            Err(err) => println!("Unrecognised command: {err}"),
         }
-
-        let result = rustemon::pokemon::pokemon::get_by_name(&input, &rustemon_client).await;
-        match result {
-            Ok(species) => {
-                for stat in species.stats.into_iter() {
-                    println!("{}: {}", stat.stat.name, stat.base_stat);
-                }
-            }
-            Err(err) => println!(
-                "Unable to fetch {} ({}): is the species name correct?",
-                input, err
-            ),
-        }
-        println!()
     }
+}
+
+async fn encounter(client: &RustemonClient, pokemon_box: &mut HashMap<String, Pokemon>) {
+    let species_name = read_in("Enter a species name:");
+
+    let result = pokemon::get_by_name(&species_name, client).await;
+    match result {
+        Ok(species) => {
+            print_species(&species);
+            let caught = read_in("Did you catch it? (Y/N):");
+
+            match caught.parse() {
+                Ok(Caught::Y) => catch(pokemon_box, species),
+                Ok(Caught::N) => println!("Better luck next time!"),
+                Err(err) => println!("Unrecognised statement: {err}"),
+            }
+        }
+        Err(err) => println!(
+            "Unable to fetch {} ({}): is the species name correct?",
+            species_name, err
+        ),
+    }
+}
+
+fn catch(pokemon_box: &mut HashMap<String, Pokemon>, species: Pokemon) {
+    let name = read_in(&format!("Enter a unique name for {}:", species.name));
+    pokemon_box.insert(name, species);
+}
+
+fn withdraw(pokemon_box: &HashMap<String, Pokemon>) {
+    let name = read_in("Withdraw who?");
+
+    match pokemon_box.get(&name) {
+        Some(species) => print_species(species),
+        None => println!("Not found"),
+    }
+}
+
+fn print_species(species: &Pokemon) {
+    for stat in species.stats.iter() {
+        println!("{}: {}", stat.stat.name, stat.base_stat);
+    }
+}
+
+fn read_in(prompt: &str) -> String {
+    let mut buffer = String::new();
+    println!("{prompt}");
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Failed to read line");
+    buffer.trim().to_owned()
 }
